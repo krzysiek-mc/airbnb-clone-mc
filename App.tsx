5dd9e7aed6b4ab9f28094e658728aa0a4fb15ff3
Agnieszka26 <medrek.agn@yahoo.com>
2022-11-20 17:10:22 +0100

    add simple page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './components/Title/Title';
+import Description from './components/Description/Description';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Title title={'Title'} />
+      <Description
+        description={
+          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+        }
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
