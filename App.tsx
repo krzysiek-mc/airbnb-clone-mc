3508a74541765ef6703bbaa8cfd84b1ba04bafee
Michał Matczuk <mm@webx.pl>
2022-11-22 14:35:37 +0100

    try dupa into git

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import { FC } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const App: FC<{ id: number; name: string; product: string }> = ({ id, name, product }) => {
+const App: FC<{ id: number; name: string; product: string }> = ({ id, name, product, dupa }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>HELLO</Text>
