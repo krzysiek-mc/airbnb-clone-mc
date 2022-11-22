bde900fdbf0c5670d6e581c9f73b6a7c5eab1a77
AleksSlawek <aleksander.slawek@mindconsulting.pl>
2022-11-22 08:25:28 +0100

    format with prettier

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Hello</Text>
       <Text style={styles.description}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error culpa at
-        reprehenderit impedit doloribus perspiciatis inventore fugiat ab quam
-        exercitationem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error culpa at reprehenderit
+        impedit doloribus perspiciatis inventore fugiat ab quam exercitationem!
       </Text>
     </View>
   );
@@ -17,9 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   header: {
     width: 300,
