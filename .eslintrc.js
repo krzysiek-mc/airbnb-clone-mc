04a4fb8e2a5b7759a57639ec0ee769e343d838aa
aleksslawek <aleksander.slawek@mindconsulting.pl>
2022-11-24 20:04:16 +0100

    Fix eslint error with import from reactnative

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,4 +11,7 @@ module.exports = {
   rules: {
     "no-unused-vars": ["error", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
   },
+  settings: {
+    "import/ignore": ["react-native"],
+  },
 };
