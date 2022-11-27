f257c818e8b24be1d4c25f204b4b5ea2f00de72c
grzegorz882 <grzegorz.tarkowski@mindconsulting.pl>
2022-11-27 16:54:14 +0100

    Fetch and display simple data and add basic configuration for debugging

--- App.tsx
+++ App.tsx
@@ -1,13 +1,50 @@
 import { StatusBar } from "expo-status-bar";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+type SimplePokemonData = {
+  name: string;
+  height: number;
+  weight: number;
+};
+
 const App: FC = () => {
+  const [data, setData] = useState<SimplePokemonData>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await (
+        await fetch("https://pokeapi.co/api/v2/pokemon/6", { method: "GET" })
+      ).json();
+
+      setData(response);
+      setLoading(false);
+    } catch (err) {
+      console.log("error from getData in App.tsx", err);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>HELLO</Text>
-      <Text>Lorem ipsum...</Text>
-      <StatusBar style="auto" />
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <Text style={styles.title}>HELLO</Text>
+          <Text>Lorem ipsum...</Text>
+          <Text>
+            {data.name} {data.height} {data.weight}
+          </Text>
+          <StatusBar style="auto" />
+        </>
+      )}
     </View>
   );
 };
